f7a10941d6ad4f7cbd8b76a7ffb74cbf341bf9fd
Frederic Vogels <frederic.vogels@ucll.be>
2020-05-29 19:37:58 +0200

    possibleCombinations

---
+++ src/solver.ts
@@ -0,0 +1,37 @@
+import { Repeat, Seq, Range } from 'immutable';
+
+
+export enum Square
+{
+    Empty,
+    Filled
+}
+
+
+export function* possibleCombinations(length : number, constraints : readonly number[], needsSpace : boolean = false) : Iterable<Seq.Indexed<Square>>
+{
+    if ( length >= 0 )
+    {
+        if ( constraints.length === 0 )
+        {
+            yield Repeat(Square.Empty, length);
+        }
+        else
+        {
+            const [ c, ...cs ] = constraints;
+            const island = Repeat(Square.Filled, c);
+
+            for ( const nSpaces of Range(needsSpace ? 1 : 0, length) )
+            {
+                const space = Repeat(Square.Empty, nSpaces);
+
+                for ( const rest of possibleCombinations(length - nSpaces - c, cs, true) )
+                {
+                    const concatenation : Seq.Indexed<Square> = space.concat<Square>(island, rest);
+
+                    yield concatenation;
+                }
+            }
+        }
+    }
+}
---
+++ tests/solver-tests.ts
@@ -0,0 +1,96 @@
+/* tslint:disable */
+
+import { expect } from 'chai';
+import { possibleCombinations, Square } from '@/solver';
+
+
+describe('possibleCombinations', function() {
+    const inputs : { length: number, constraints : number[], expected : string[] }[] = [
+        {
+            length: 0,
+            constraints: [],
+            expected: [ "" ]
+        },
+        {
+            length: 1,
+            constraints: [],
+            expected: [ "." ]
+        },
+        {
+            length: 2,
+            constraints: [],
+            expected: [ ".." ]
+        },
+        {
+            length: 1,
+            constraints: [1],
+            expected: [ "x" ]
+        },
+        {
+            length: 2,
+            constraints: [1],
+            expected: [ "x.", ".x" ]
+        },
+        {
+            length: 3,
+            constraints: [1],
+            expected: [ "..x", ".x.", "x.." ]
+        },
+        {
+            length: 2,
+            constraints: [2],
+            expected: [ "xx" ]
+        },
+        {
+            length: 3,
+            constraints: [1, 1],
+            expected: [ "x.x" ]
+        },
+        {
+            length: 2,
+            constraints: [3],
+            expected: [ ]
+        },
+        {
+            length: 3,
+            constraints: [2],
+            expected: [ "xx.", ".xx" ]
+        },
+        {
+            length: 4,
+            constraints: [1, 1],
+            expected: [ "x..x", "x.x.", ".x.x" ]
+        },
+        {
+            length: 4,
+            constraints: [1, 2],
+            expected: [ "x.xx" ]
+        },
+        {
+            length: 5,
+            constraints: [1, 2],
+            expected: [ "x.xx.", "x..xx", ".x.xx" ]
+        },
+        {
+            length: 4,
+            constraints: [4],
+            expected: [ "xxxx" ]
+        },
+    ];
+
+    inputs.forEach(function ({length, constraints, expected: expectedStrings}) {
+        const expected = expectedStrings.map(parse);
+
+        it(`length ${length}, constraints [${constraints}]`, function() {
+            const actual = [...possibleCombinations(length, constraints)].map(xs => [...xs]);
+
+            expect(actual.length).to.be.equal(expected.length);
+            expect(actual).to.have.same.deep.members(expected);
+        });
+    });
+
+    function parse(s: string) : Square[]
+    {
+        return s.split('').map(c => c === '.' ? Square.Empty : Square.Filled);
+    }
+});
